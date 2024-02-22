4f0055c4c795704a88b91f432125c5b9e9810ba7
nguyenduybach12a11@gmail.com <nguyenduybach12a11@gmail.com>
2024-02-22 14:49:25 +0000

    design form v4

--- client_code/Form1/InsertProcess/__init__.py
+++ client_code/Form1/InsertProcess/__init__.py
@@ -1,15 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import InsertProcessTemplate
 from anvil import *
-import plotly.graph_objects as go
 
-class Form1(Form1Template):
+class InsertProcess(InsertProcessTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def repeating_panel_process_show(self, **event_args):
-    pass
-
-
