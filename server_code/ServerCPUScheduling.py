046010f480fc000ec406157e38ab47b6d0555a8d
nguyenduybach12a11@gmail.com <nguyenduybach12a11@gmail.com>
2024-02-23 18:21:43 +0000

    Edited ServerCPUScheduling

--- server_code/ServerCPUScheduling.py
+++ server_code/ServerCPUScheduling.py
@@ -31,6 +31,27 @@ def roundRobinScheduling(processList, quantumTime):
       timeLine = timeLine + processReady['bt']
 
   return ganttGraph
-  
 
-  
+@anvil.server.callable
+def FCFSScheduling(processList):
+  ganttGraph = []
+  processListSortedByAT = sorted(processList, key=lambda item: item['at'])
+  timeLine = 0
+  processQueue = queue.Queue()
+  for process in processListSortedByAT:
+    processQueue.put(process)
+
+  while(processQueue.empty() == False):
+    processReady = processQueue.get()
+    
+    if(processReady['bt'] > quantumTime):
+      processReady['bt'] = processReady['bt'] - quantumTime
+      ganttGraph.append({'name': processReady['process'], 'time-start': str(timeLine), 'time-end': str(timeLine + quantumTime)})
+      timeLine = timeLine + quantumTime
+      processQueue.put(processReady)
+      
+    else:
+      ganttGraph.append({'name': processReady['process'], 'time-start': str(timeLine), 'time-end': str(timeLine + processReady['bt'])})
+      timeLine = timeLine + processReady['bt']
+
+  return ganttGraph
